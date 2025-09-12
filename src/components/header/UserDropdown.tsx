a8ee7d38b874586c777e755285ded3ca54cc41e6
TIN Wizard <70106464+tindevelopers@users.noreply.github.com>
2025-09-13 01:21:58 +0200

    Fix ESLint error in UserDropdown component
    - Remove unused Link import since sign out is now a button

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
